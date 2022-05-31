74a7e6620dda4d8d0dee5ef158d20a287a1b299e
tyuhbfjhjvcdx <102887397+tyuhbfjhjvcdx@users.noreply.github.com>
2022-05-31 01:56:45 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,10 +2,10 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('路径')) {
-      url.hostname = 'sclzp-doyousdnvuzgh.koyeb.app'
+      url.hostname = 'fengzhishang01.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
     return env.ASSETS.fetch(request);
   },
-};
+}
